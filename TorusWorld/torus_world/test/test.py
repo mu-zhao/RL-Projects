2d9441adb3d5f4ee44886983cecc78008fd1d928
muzhao <muzhao@didiglobal.com>
2021-10-15 14:17:42 -0700

    merge

--- TorusWorld/torus_world/test/test.py
+++ TorusWorld/torus_world/test/test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #%%
 from common.map_editor import random_generate_map
 #%%
@@ -10,7 +9,3 @@ drift_config = [3,0.2]
 path = 'TorusWorld/data/maps'
 #%%
 random_generate_map(map_id, size, end_loc, reward_config, drift_config,path)
-=======
-from common.flat_torus import FlatTorus as FT
-path = 
->>>>>>> 6237248fd0178794e6a04b6d1c4e3e787df15b0d
