fb54ba91b6b501e41b0f79bbcecb4e4f6481b517
muzhao <muzhao@didiglobal.com>
2021-10-17 18:01:21 -0700

    fd

--- TorusWorld/torus_world/common/flat_torus.py
+++ TorusWorld/torus_world/common/flat_torus.py
@@ -1,5 +1,5 @@
 import logging
-from multiprocessing import Pool
+from multiprocessing import Pool, cpu_count
 from time import time
 import numpy as np
 
@@ -8,30 +8,36 @@ from common.episode_utils import Episode
 logger = logging.getLogger(__name__)
 
 
+def run_episode(episode, algo, train=True):
+    episode.reset(algo, train)
+    if train:
+        algo.hyper_parameters.trained_episode += 1
+    while True:
+        if episode.episode_end:
+            return episode.reward
+        episode.update(algo, train)
+
+
+def evaluate_algo(algo, num_runs, config, t):
+    p = Pool(cpu_count()-4)
+    print('create pool', time()-t)
+    res = [p.apply_async(run_episode, args=(
+           Episode(config.params, config.torus_map), algo, False))
+           for _ in range(num_runs)]
+    print('assign', time()-t)
+    p.close()
+    p.join()
+    print('begore', time()-t)
+    res = [r.get() for r in res]
+    print('last', time()-t)
+    return np.array(res)
+
+
 class FlatTorus:
     def __init__(self, world_config):
         self.config = world_config
         self._num_episodes = self.config.params.episodes
         self._cur_episode = Episode(self.config.params, self.config.torus_map)
-
-    def run_episode(self, train=True):
-        self._cur_episode.reset(self.config.algo, train)
-        if train:
-            self.config.algo.hyper_parameters.trained_episode += 1
-        while True:
-            if self._cur_episode.episode_end:
-                return self._cur_episode.reward
-            self._cur_episode.update(self.config.algo, train)
-
-    def evaluate_algo(self, num_runs):
-
-        def _eval_algo():
-            return self.run_episode(False)
-        p = Pool()
-        res = [p.apply_async(_eval_algo, args=()) for _ in range(num_runs)]
-        p.close()
-        p.join()
-        return np.array([r.get() for r in res])
 
     def train_evaluation(self, eval_gap=10000, num_runs=10000):
         self.time = time()
@@ -43,13 +49,13 @@ class FlatTorus:
             if trained_episode > 0 and trained_episode % eval_gap == 0:
                 t=time()
                 print('train time ', t-self.time)
-                eval_result = self.evaluate_algo(num_runs)
+                eval_result = evaluate_algo(self.config.algo, num_runs,self.config, t)
                 self.time = time()
                 print('eval_time ', self.time -t)
                 mean, var = eval_result.mean(), eval_result.std()
                 self._return_record.append([mean, var])
                 print(trained_episode, mean, var)
-            self.run_episode()  # training
+            run_episode(self._cur_episode, self.config.algo)  # training
         return self._return_record
 
     def add_train_episodes(self, num_episodes):
